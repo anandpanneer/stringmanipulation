c457e6f37504bb242d8e8fd5326bb8beb035ad29
Anand <anand.panneer@gmail.com>
2016-12-06 12:23:15 +0530

    First commit

---
+++ src/com/alternatecase/StringUtils.java
@@ -0,0 +1,26 @@
+package com.alternatecase;
+
+public class StringUtils {
+
+	public static String toAlternativeString(String input) {
+		String temp = input;
+		return convertToUpperOrLower(temp);
+	}
+
+	private static String convertToUpperOrLower(String input) {
+		StringBuilder result = new StringBuilder();
+		for (char eachLetter : input.toCharArray()) {
+			if (Character.isUpperCase(eachLetter)) {
+				result.append(Character.toLowerCase(eachLetter));
+			}
+			else if (Character.isLowerCase(eachLetter)) {
+				result.append(Character.toUpperCase(eachLetter));
+			}
+			else {
+				result.append(eachLetter);
+			}
+		}
+		return result.toString();
+	}
+
+}
